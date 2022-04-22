685bc5e0078594b3b238479be49fcd0d43756727
Adrian Kaluzinski <akaluzin@akamai.com>
2022-04-22 17:00:00 +0200

    Refactor to ES6 and add new menu items

---
+++ apps/menu-generator.js
@@ -0,0 +1,169 @@
+const days = 7;
+const defaultMealDays = 2;
+
+const lunchMeals = [
+    {
+        name: "Rosół",
+        isWeekendOnly: true
+    },
+    {
+        name: "Leczo",
+        isWeekendOnly: false
+    },
+    {
+        name: "Lazania",
+        isWeekendOnly: true
+    },
+    {
+        name: "Łazanki"
+    },
+    {
+        name: "Kurczak w szynce parmeńskiej"
+    },
+    {
+        name: "Pieczone udka",
+        isWeekendOnly: true
+    },
+    {
+        name: "Kurczak w papirusie",
+        days: 1
+    },
+    {
+        name: "Papryka nadziewana ryżem i mięsem mielonym"
+    },
+    {
+        name: "Chinczyk a la Adi"
+    },
+    {
+        name: "Łosoś teryiaki z mango",
+        days: 1,
+        isWeekendOnly: true
+    },
+    {
+        name: "Fix Bolognese"
+    },
+    {
+        name: "Carbonarra",
+        days: 1,
+        isWeekendOnly: true
+    },
+    {
+        name: "Kurczak z ricottą",
+        days: 2
+    },
+    {
+        name: "Risotto"
+    },
+    {
+        name: "Mielone"
+    },
+    {
+        name: "Kotlet z fileta kurczaka",
+        isWeekendOnly: true
+    },
+    {
+        name: "Kotleciki warzywne"
+    },
+    {
+        name: "Kurczak z salsą avocado"
+    },
+    {
+        name: "Pizza",
+        isWeekendOnly: true
+    },
+    {
+        name: "Chilli con carne"
+    },
+    {
+        name: "Kurczak teriyaki z ryżem brązowym"
+    },
+    {
+        name: "Risotto z kurczakiem i dynią"
+    },
+    {
+        name: "Butter Chicken"
+    },
+    {
+        name: "Filety z kurczaka w sosie pieczarkowym"
+    },
+    {
+        name: "Rolada, gumiklyjzy i modro",
+        isWeekendOnly: true
+    }
+];
+
+const soups = ["Grzybowa", "Ogórkowa", "Barszcz", "Szczawiowa", "Żurek", "Pomidorowa", "Szparagowa"]
+const dinners = ["Kiełbaska z grilla", "Kanapki", "Szakszuka"];
+
+const feedMe = possibleMeals => possibleMeals[Math.floor(Math.random() * possibleMeals.length)];
+
+const setDefaultDays = lunch => ({
+    ...lunch,
+    days: lunch.days ?? defaultMealDays
+});
+
+function generateLunchMenu(tillDays = days) {
+
+    let lunches = [];
+    let covered = 0;
+    do {
+        const allMeals = lunchMeals.filter(({name}) => !lunches.includes(name)).map(lunch => setDefaultDays(lunch));
+        const weekdayMeals = allMeals.filter(lunches => !lunches.isWeekendOnly);
+        const weekendMeals = allMeals.filter(({isWeekendOnly}) => isWeekendOnly);
+        const isWeekend = covered > 5;
+        const pickedMeal = feedMe(isWeekend ? weekendMeals : weekdayMeals);
+
+        for (let i = 0; i < pickedMeal.days; i++) {
+            lunches.push(pickedMeal.name);
+        }
+
+        covered += pickedMeal.days;
+    } while (covered < tillDays);
+
+    return lunches;
+}
+
+const lunchMenu = generateLunchMenu();
+
+const body = document.getElementById("tableDiv");
+
+const tbl = document.createElement("table");
+const tblBody = document.createElement("tbody");
+
+const daysOfWeek = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela", "Poniedziałek"];
+const headers = ["Dzień", "Drugie danie", "Zupa", "Kolacja"]
+
+const headersRow = document.createElement("tr");
+headers.forEach(item => {
+    const cell = document.createElement("td");
+    cell.appendChild(document.createTextNode(item));
+    cell.className = "bg-info";
+    headersRow.appendChild(cell);
+});
+
+tblBody.appendChild(headersRow);
+
+for (let i = 0; i < lunchMenu.length; i++) {
+    let row = document.createElement("tr");
+
+    const dayCell = document.createElement("td");
+    const lunchCell = document.createElement("td");
+    const soupCell = document.createElement("td");
+    const dinnerCell = document.createElement("td");
+
+    dayCell.appendChild(document.createTextNode(daysOfWeek[i]));
+    lunchCell.appendChild(document.createTextNode(lunchMenu[i]));
+    soupCell.appendChild(document.createTextNode(feedMe(soups)));
+    dinnerCell.appendChild(document.createTextNode(feedMe(dinners)));
+
+    row.appendChild(dayCell);
+    row.appendChild(lunchCell);
+    row.appendChild(soupCell);
+    row.appendChild(dinnerCell);
+
+    tblBody.appendChild(row);
+}
+
+tbl.appendChild(tblBody);
+body.appendChild(tbl);
+tbl.setAttribute("class", "table table-bordered table-striped table-responsive mx-auto");
